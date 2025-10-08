6a95e2c92a61705221d6942a8acfa79b6aa722b1
Soufiane-G <24-00378.gha.sou@isfce.be>
2025-10-08 23:24:32 +0200

    texte côté droit V1.3 LocalStorage V1

--- ProjetWeb/Projet BilSouf/index.js
+++ ProjetWeb/Projet BilSouf/index.js
@@ -1,34 +1,45 @@
-window.addEventListener("DOMContentLoaded", function (event) {
+window.addEventListener("DOMContentLoaded", function () {
 
+    /*Bonus de sauvegarde de la couleur*/
+    const savedColor = localStorage.getItem("text-color");
+    const savedFont=localStorage.getItem("fontSize");
+    if (savedColor ) {
+        document.documentElement.style.setProperty("--text-color", savedColor);
+    }
+    if(savedFont){
+        document.documentElement.style.setProperty=savedFont;
+    }
+    /*code JC pour le selecteur*/
     const selecteur = document.getElementById("selecteur");
     if (selecteur) {
         selecteur.innerHTML = `<label for="color-select">Couleur : </label>
-
         <select id="color-select">
-
-            <option value="#000000"> NOIR</option>
+            <option value="#000000" > NOIR </option>
             <option value="#0000FF"> BLEU</option>
-            <option value="#FF0000"> ROUGE</option>
-
+            <option value="#FF0000" > ROUGE</option>
         </select>
         <label for="police-select">Taille : </label>
-
         <select id="police-select">
-
-            <option value="12px"> PETIT</option>
+            <option value="10px"> PETIT</option>
             <option value="16px"> MOYEN</option>
             <option value="24px"> GRAND</option>
-
         </select>`;
     }
     const color = document.getElementById("color-select")
-    color.addEventListener("change", function (event) {
-        document.documentElement.style.setProperty("--text-color", color.value)
-    })
+    if(color) {
+        color.addEventListener("change", function () {
+            document.documentElement.style.setProperty("--text-color", color.value);
+            localStorage.setItem("text-color", color.value)
+        })
+    }
+
     const police = document.getElementById("police-select")
-    police.addEventListener("change", function (event) {
-        document.documentElement.style.fontSize = police.value
-    })
+    if(police) {
+        police.addEventListener("change", function () {
+            document.documentElement.style.fontSize = police.value
+            localStorage.setItem("policeSize", police.value);
+        })
+    }
 
     const article = document.getElementById("article-main"); //Récupère l'élément de <article>.
     const bouton = document.getElementById("masquer-article"); // récupère l'élement de <button>.
@@ -36,7 +47,7 @@ window.addEventListener("DOMContentLoaded", function (event) {
 
         if (article.style.display !== "none") { // pour vérifier si l'article n'est pas vide donc "visible" ici
             article.style.display = "none";       // On cache l'article
-            bouton.textContent = "Afficher l'article"; // Le bouton change et se nomme "Afficher l'article"
+            bouton.textContent = "Afficher l'article"; // Le bouton change et se nomme "Afficher l'article."
         } else {
             article.style.display = "block";            // si l'article est visible :
             bouton.textContent = "Masquer l'article";  //le bouton devient masquer l'article
@@ -45,15 +56,15 @@ window.addEventListener("DOMContentLoaded", function (event) {
     })
 
 
-    /*affichage des infos en haut a droite */
+    /*affichage des infos en haut à droite */
     const card = document.querySelectorAll('.info-art');              // toute la carte
     const position = document.getElementById("position");                  // position de l'information affo-ichée
     const msg = " "
 
-    /*création d'un fonction afin de pouvoir la réutiliser plus tard
-    * récupération de tous le contenu des articles dont la class est .info-art ensuite on récupère
-    * le text-muted de larticle qui aura provoquer l'event
-    * on retourne les infos dans la position ou on laisse le cadre vide */
+    /*création d'une fonction afin de pouvoir la réutiliser plus tard
+    * récupération de tout le contenu des articles dont la class est .info-art ensuite l'on récupère
+    * le "text-tmuted" de l'article qui aura provoqué l'event
+    * Les infos sont retournées dans la position ou on laisse le cadre vide */
     function over(event) {
         const art = event.currentTarget.querySelector(".text-muted");
         if(art) {
