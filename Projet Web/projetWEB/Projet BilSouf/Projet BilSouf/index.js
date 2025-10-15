cc1c6f4e7fe689455c0a7b02eb9b075d29358e45
Bilain Jaber <rekkleselement@hotmail.com>
2025-10-15 13:18:28 +0200

    Ajout formulaire HTML

---
+++ Projet Web/projetWEB/Projet BilSouf/Projet BilSouf/index.js
@@ -0,0 +1,94 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    /*Bonus de sauvegarde de la couleur*/
+    const savedColor = localStorage.getItem("text-color");
+    const savedFont=localStorage.getItem("fontSize");
+    if (savedColor ) {
+        document.documentElement.style.setProperty("--text-color", savedColor);
+    }
+    if(savedFont){
+        document.documentElement.style.setProperty=savedFont;
+    }
+    /*code JC pour le selecteur*/
+    const selecteur = document.getElementById("selecteur");
+    if (selecteur) {
+        selecteur.innerHTML = `<label for="color-select">Couleur : </label>
+        <select id="color-select">
+            <option value="#000000" > NOIR </option>
+            <option value="#0000FF"> BLEU</option>
+            <option value="#FF0000" > ROUGE</option>
+        </select>
+        <label for="police-select">Taille : </label>
+        <select id="police-select">
+            <option value="10px"> PETIT</option>
+            <option value="16px"> MOYEN</option>
+            <option value="24px"> GRAND</option>
+        </select>`;
+    }
+    const color = document.getElementById("color-select")
+    if(color) {
+        color.addEventListener("change", function () {
+            document.documentElement.style.setProperty("--text-color", color.value);
+            localStorage.setItem("text-color", color.value)
+        })
+    }
+
+    const police = document.getElementById("police-select")
+    if(police) {
+        police.addEventListener("change", function () {
+            document.documentElement.style.fontSize = police.value
+            localStorage.setItem("policeSize", police.value);
+        })
+    }
+    //Affichez/Masquer l'article
+    const article = document.getElementById("article-main"); //Récupère l'élément de <article>.
+    const bouton = document.getElementById("masquer-article"); // récupère l'élement de <button>.
+    bouton.addEventListener("click", function () {
+
+        if (article.style.display !== "none") { // pour vérifier si l'article n'est pas vide donc "visible" ici
+            article.style.display = "none";       // On cache l'article
+            bouton.textContent = "Afficher l'article"; // Le bouton change et se nomme "Afficher l'article."
+        } else {
+            article.style.display = "block";            // si l'article est visible :
+            bouton.textContent = "Masquer l'article";  //le bouton devient masquer l'article
+        }
+
+    })
+
+
+    /*affichage des infos en haut à droite */
+    const card = document.querySelectorAll('.info-art');              // toute la carte
+    const position = document.getElementById("position");                  // position de l'information affo-ichée
+    const msg = " "
+
+    /*création d'une fonction afin de pouvoir la réutiliser plus tard
+    * récupération de tout le contenu des articles dont la class est .info-art ensuite l'on récupère
+    * le "text-muted" de l'article qui aura provoqué l'event
+    * Les infos sont retournées dans la position ou on laisse le cadre vide */
+    function over(event) {
+        const art = event.currentTarget.querySelector(".text-muted");
+        if(art) {
+            position.innerHTML = art.innerText;
+        }
+        else{
+            position.innerHTML = msg
+        }
+    }
+
+    function out() {
+        position.innerHTML = msg;
+    }
+
+    card.forEach(elem=> {
+    elem.addEventListener("mouseover", over);
+    elem.addEventListener("mouseout", out);
+})
+
+})
+
+
+/*commentaire test pour le commit*/
+
+
+
+
